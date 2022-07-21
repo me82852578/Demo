3171b35040860f850a75d157ad19d83b746bb1b4
Jay <me82852578@gmail.com>
2022-07-22 02:59:07 +0800

    [feat] add login page && facebook login

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,30 @@ import { Home, LineLiffSearchStore, Login, SignUp } from "./pages";
 
 const App = () => {
   // return <Home />;
+  window.fbAsyncInit = function () {
+    FB.init({
+      appId: process.env.REACT_APP_NAME_FACEBOOK_APP_ID,
+      xfbml: true,
+      version: "v14.0",
+    });
+    FB.AppEvents.logPageView();
+    FB.getLoginStatus(function (response) {
+      console.info(response);
+    });
+  };
+
+  (function (d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+      return;
+    }
+    js = d.createElement(s) as HTMLScriptElement;
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode?.insertBefore(js, fjs);
+  })(document, "script", "facebook-jssdk");
+
   const darkTheme = createTheme({
     palette: {
       // mode: "light",
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,110 @@
 import React from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { LockOutlined } from "@mui/icons-material";
+import FacebookLogin, {
+  ReactFacebookFailureResponse,
+} from "react-facebook-login";
 
-const Login = () => {
-  return <div>Login</div>;
-};
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
-export default Login;
+export default function SignIn() {
+  const handleFacebookLoginOnClick = () => {};
+  const handleFacebookLoginCallback = (res: ReactFacebookFailureResponse) => {
+    console.info(res);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: "16px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar>
+          <LockOutlined />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <FacebookLogin
+          appId={process.env.REACT_APP_NAME_FACEBOOK_APP_ID as string}
+          autoLoad={true}
+          fields="name,email,picture"
+          onClick={handleFacebookLoginOnClick}
+          callback={handleFacebookLoginCallback}
+        />
+        <form noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button type="submit" fullWidth variant="contained" color="primary">
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </Box>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
